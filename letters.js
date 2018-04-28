277e2374896032ccf9338cdf7074263daaa69de1
Ryan Stowe <ryanwstowe@gmail.com>
2018-04-27 22:47:30 -0400

    not really working yet

---
+++ letters.js
@@ -0,0 +1,17 @@
+let x = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p",
+"q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+// empty array
+
+
+// let alphabet = function (letters) {
+//   this.letters = letters;
+//   this.upper= false;
+// };
+
+x.forEach(function () {
+  x.toString();
+  // return x.prototype.toUpperCase();
+
+});
+
+console.log(x);
---
+++ words.js
@@ -0,0 +1,22 @@
+
+const words = ['poop', 'goofy', 'bachelor'];
+
+let selector = function () {
+ math.random()*3;
+};
+
+// let alphabet = function (words) {
+//   this.letters = letters;
+//   this.blankSpace =
+//   this.upper= false;
+// };
+//
+//
+// alphabet.prototype.toString = function (words) {
+//   let guess =
+//
+// }
+
+// console.log(word.push());
+
+return selector();
